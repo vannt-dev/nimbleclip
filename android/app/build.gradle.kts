3bf5a691c5fdbb25789fe05297a60f387196ea92
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-08-18 23:18:13 +0700

    fix(build): configure Android compileSdk 36 and gradle properties for multi-drive support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.snapvideo.snap_video"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
